c4cb82bc5a4e99c0bf7a62bf9ac7932708221be2
Sebbe213 <sebastianraoof@hotmail.se>
2025-03-26 12:34:50 +0100

    Added roles to the Ai and made a function to be used

--- AI-model/server/RAG.py
+++ AI-model/server/RAG.py
@@ -4,16 +4,13 @@ from dotenv import load_dotenv
 from pathlib import Path
 
 # Load .env.local file from current directory
-load_dotenv(Path(".env.local"))
-# Set the API key
-openai.api_key = os.getenv('API_KEY')
+def ai_model(prompt, tokens ):
+    load_dotenv(Path(".env.local"))
+    # Set the API key
+    openai.api_key = os.getenv('API_KEY')
 
-# Make a request using a chat model
-response = openai.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "user", "content": "write a one sentence story about a dog."}
-    ]
-)
 
-print(response.choices[0].message.content)
+    # Make a request using a chat model
+    response = openai.chat.completions.create(model="gpt-3.5-turbo", messages=[{"role": "system" ,"content": "....."},{"role": "user","content": prompt}], max_tokens = tokens)
+
+    print(response.choices[0].message.content)
