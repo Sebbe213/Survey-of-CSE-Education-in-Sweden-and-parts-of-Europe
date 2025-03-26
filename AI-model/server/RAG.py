1269e27f90d5bbdefaa0f257485b00543ad6342e
Sebbe213 <sebastianraoof@hotmail.se>
2025-03-26 13:44:11 +0100

    Removed comments

--- AI-model/server/RAG.py
+++ AI-model/server/RAG.py
@@ -6,11 +6,10 @@ from pathlib import Path
 # Load .env.local file from current directory
 def ai_model(prompt, tokens ):
     load_dotenv(Path(".env.local"))
-    # Set the API key
     openai.api_key = os.getenv('API_KEY')
 
 
-    # Make a request using a chat model
     response = openai.chat.completions.create(model="gpt-3.5-turbo", messages=[{"role": "system" ,"content": "....."},{"role": "user","content": prompt}], max_tokens = tokens)
-
     print(response.choices[0].message.content)
+    return response.choices[0].message.content
+
