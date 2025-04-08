349bd64ab529303669a10291d6d83e09306236ae
Sebbe213 <sebastianraoof@hotmail.se>
2025-04-08 10:29:26 +0200

    All tables are now added.

--- AI-model/server/prepare_embeddings.py
+++ AI-model/server/prepare_embeddings.py
@@ -77,7 +77,74 @@ University College London (UCL),England,75,2022,UG,15,University-specific,CS/IT
 Institut Polytechnique de Paris (IP Paris) (Data from École Polytechnique),France,100,2019,PG,6,University-specific,Engineering
 Riga Technical University (RTU),Latvia,88,2019,PG,12,National,All
 University of Tartu (UT),Estonia,87,2019,UG,6,University-specific,All
-University of Tartu (UT),Estonia,94,2019,PG,6,University-specific,All"""]
+University of Tartu (UT),Estonia,94,2019,PG,6,University-specific,All""",
+"""University Name,International Outlook,Industry Income,Source
+Chalmers University of Technology (Sweden),77.4,66.0,THE World University Rankings 2023
+University of Gothenburg (Sweden),64.4,77.3,THE World University Rankings 2025
+KTH Royal Institute of Technology (Sweden),82.9,97.3,THE World University Rankings 2025
+Norwegian University of Science and Technology (Norway),66.3,72.3,THE World University Rankings 2025
+Universitat Politècnica de València (Spain),57.7,N/A,THE World University Rankings 2025
+Gdańsk University of Technology (Poland),39.4,N/A,THE World University Rankings 2025
+Warsaw University of Technology (Poland),32.8,54.9,THE World University Rankings 2025
+Politecnico di Milano (Italy),64.4,96.3,THE World University Rankings 2024
+RWTH Aachen University (Germany),70.3,100.0,THE World University Rankings 2024
+Technische Universität Berlin (Germany),N/A,N/A,Not listed in latest THE rankings
+Technical University of Munich (Germany),83.1,100.0,THE World University Rankings 2024
+ETH Zurich (Switzerland),95.3,84.0,THE World University Rankings 2025
+EPFL (Switzerland),95.3,100.0,THE World University Rankings 2025
+University of Copenhagen (Denmark),79.3,90.4,THE World University Rankings 2025
+University of Helsinki (Finland),59.2,68.2,THE World University Rankings 2025
+University of Cambridge (England),97.1,88.4,THE World University Rankings 2025
+University of Oxford (England),97.3,99.6,THE World University Rankings 2025
+University College London (England),97.7,74.9,THE World University Rankings 2025
+Institut Polytechnique de Paris (France),97.0,98.3,THE World University Rankings 2024
+Riga Technical University (Latvia),N/A,N/A,Not listed in latest THE rankings
+University of Tartu (Estonia),N/A,N/A,Not listed in latest THE rankings
+""","""University,Overall Satisfaction,Teaching Quality,Campus Facilities,Support Services,Social Experience
+Chalmers Univ. of Technology,90%,90%,85-90%,90%,85%
+Univ. of Gothenburg,89%,89%,90%,91%,89%
+KTH Royal Inst. of Technology,80%,N/A,N/A,N/A,N/A
+NTNU (Norwegian Univ. of Sci. & Tech.),82%,N/A,N/A,N/A,N/A
+Univ. Politécnica de Valencia (UPV),88%,N/A,N/A,N/A,N/A
+Gdańsk Univ. of Technology,80%,N/A,N/A,N/A,N/A
+Warsaw Univ. of Technology (PW),78%,N/A,N/A,N/A,N/A
+Politecnico di Milano,86%,N/A,N/A,N/A,N/A
+RWTH Aachen University,76%,97%,N/A,82%,N/A
+TU Berlin,88%,N/A,N/A,N/A,N/A
+Technical Univ. of Munich (TUM),82%,N/A,N/A,N/A,N/A
+ETH Zurich (Switzerland),62%,N/A,N/A,N/A,84%
+EPFL (Switzerland),N/A,N/A,N/A,N/A,N/A
+Univ. of Copenhagen (Denmark),N/A,N/A,N/A,N/A,N/A
+Univ. of Helsinki (Finland),N/A,90%,N/A,N/A,N/A
+Univ. of Cambridge (UK),91%,90%,94%,84%,75%
+Univ. of Oxford (UK),91%,96%,97%,96%,89%
+University College London (UCL),80%,N/A,N/A,N/A,N/A
+Institut Polytechnique de Paris (France),N/A,N/A,N/A,N/A,N/A
+Riga Technical Univ. (Latvia),N/A,N/A,N/A,N/A,N/A
+Univ. of Tartu (Estonia),90%,95%,N/A,98%,63%
+""","""University,Country,Student-to-Teacher Ratio
+Chalmers University of Technology,Sweden,11.1
+University of Gothenburg,Sweden,11.5
+KTH Royal Institute of Technology,Sweden,15.6
+Norwegian University of Science and Technology,Norway,14.8
+Universitat Politècnica de València,Spain,15.2
+Gdańsk University of Technology,Poland,19.8
+Warsaw University of Technology,Poland,10.9
+Politecnico di Milano,Italy,24.4
+Rheinisch-Westfälische Technische Hochschule Aachen,Germany,37.1
+Technische Universität Berlin,Germany,48.2
+Technical University of Munich,Germany,41.8
+ETH Zurich,Switzerland,15.8
+EPFL,Switzerland,12.5
+University of Copenhagen,Denmark,4.2
+University of Helsinki,Finland,16.5
+University of Cambridge,England,11.5
+University of Oxford,England,10.8
+University College London,England,11.1
+Institut Polytechnique de Paris,France,6.7
+Riga Technical University,Latvia,31.3
+University of Tartu,Estonia,8.6
+"""]
     return data
 
 
