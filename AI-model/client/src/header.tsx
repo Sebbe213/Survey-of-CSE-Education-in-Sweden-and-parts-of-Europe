13cca60f6dd4df4e64f61bc937719ab004b00c8b
Sebbe213 <sebastianraoof@hotmail.se>
2025-04-23 19:16:27 +0200

    Made the answer window nicer looking. Made so that the input bar can not be spammed.

--- AI-model/client/src/header.tsx
+++ AI-model/client/src/header.tsx
@@ -7,10 +7,10 @@ function Header(){
         <nav className='header d-flex'>
             <Link to='/' ><img src={logo} className='logo-chalmers' alt='Home button'/></Link>
             <div className='nav-content justify-content-center'>
-                <ul className='nav-list d-flex justify-content-center'>
-                    <li><Link to="/" style={{ textDecoration: 'none' }}>Home</Link></li>
-                    <li><Link to="/Data" style={{ textDecoration: 'none' }}>Data</Link></li>
-                    <li><Link to="/About us" style={{ textDecoration: 'none' }} >About us</Link></li>
+                <ul className='nav-list d-flex justify-content-center' >
+                    <li className='list-item'><Link to="/" style={{ textDecoration: 'none' , color:"white" }}>Home</Link></li>
+                    <li className='list-item'><Link to="/Data" style={{ textDecoration: 'none' , color:"white" }}>Data</Link></li>
+                    <li className='list-item'><Link to="/About us" style= {{ textDecoration: 'none' , color:"white" }} >About us</Link></li>
                 </ul>
             </div>
         </nav>
--- AI-model/client/src/mainpage.tsx
+++ AI-model/client/src/mainpage.tsx
@@ -1,32 +1,47 @@
 import Header from './header';
 import {useState} from "react";
 import './mainpage.css'
+import Questionmodal from './question'
 import arrowIcon from './assets/arrow 1 (1).png';
+//TODO:
+//      - Fix the header so that it has a border
+//      - Fix the input bar so that it looks better
+//       - Fix the answer box so that it looks better
+function Mainpage() {
+    const [question, setQuestion] = useState<string>();
+    const [answer, setAnswer] = useState<string>();
+    const [list, setlist] = useState<{ question: JSX.Element; answer: JSX.Element }[]>([]);
 
-function Mainpage(){
-    const [question,setQuestion] = useState<string>();
-    const [answer,setAnswer] = useState<string>();
-    const [list , setlist] = useState<JSX.Element[]>([]);
-
-    function Promptlist(){
-        setlist(prevList => [...prevList, <p>Question: {question}</p>, <p> Answer: {answer}</p>]);
+    function Promptlist() {
+        setlist((prevList: any) => [...prevList,
+            {question: <Questionmodal question={question}/>, answer: <p className='answer'>Answer: {answer}</p>}])
+        setQuestion('')
 
     }
-
-    //Make question and answer two separate fields (components). These are then added to a list when a button is pressed and later displayed.
-    return(
-            <div>
-                <Header/>
-                <div className='answer-area justify-content-center '>
-                    <ul  style={{ listStyle: 'none' }}>
-                        {list.map( (item,index)  => (<li key={index}>{item}</li>))}
+    function Getanswerbutton(){
+        if(question === undefined || question === ''){
+            return(<button className='prompt-button' ><img className='arrow' src={arrowIcon} alt='submit answer button'/></button>)
+        }
+        return(
+            <button className='prompt-button' onClick={Promptlist}><img className='arrow' src={arrowIcon} alt='submit answer button'/></button>
+        )
+    }
+    return (
+        <body>
+        <Header/>
+            <div className='answer-area  justify-content-center align-items-center'>
+                <ul style={{listStyle: 'none'}}>
+                    {list.map((item,index)  => (<li key={index}> {item.question}
+                        {item.answer} <hr/> </li> ))}
                     </ul>
                 </div>
                 <div className='d-flex justify-content-center '>
                     <input className='input-bar' placeholder='Write your question here!' value={question} onChange={(e) => setQuestion(e.target.value) }/>
-                    <button className='prompt-button' onClick={Promptlist}><img className='arrow' src={arrowIcon} alt='submit answer button'/></button>
+                    <Getanswerbutton/>
                 </div>
-            </div>
+        </body>
+
+
 
 
     )
