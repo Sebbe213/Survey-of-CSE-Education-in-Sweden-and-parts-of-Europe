6ab6195a7ac151cc3fee09f64af5fb4946a209d9
Sebbe213 <sebastianraoof@hotmail.se>
2025-04-24 17:45:23 +0200

    Now the conversation is saved and can also be reset

--- AI-model/client/src/mainpage.tsx
+++ AI-model/client/src/mainpage.tsx
@@ -1,5 +1,5 @@
 import Header from './header';
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import './mainpage.css'
 import Questionmodal from './question'
 import arrowIcon from './assets/arrow 1 (1).png';
@@ -10,14 +10,31 @@ import arrowIcon from './assets/arrow 1 (1).png';
 function Mainpage() {
     const [question, setQuestion] = useState<string>();
     const [answer, setAnswer] = useState<string>();
-    const [list, setlist] = useState<{ question: JSX.Element; answer: JSX.Element }[]>([]);
+    const [list, setlist] = useState< { question: string; answer: string }[]>([]);
+
+    useEffect(() => {
+        const storedList = localStorage.getItem("list");
+        if(storedList){
+            const parsedList = JSON.parse(storedList);
+            setlist(parsedList);
+        }
+    },[]);
+
+
+
 
     function Promptlist() {
-        setlist((prevList: any) => [...prevList,
-            {question: <Questionmodal question={question}/>, answer: <p className='answer'>Answer: {answer}</p>}])
-        setQuestion('')
-
+        setlist((prevList: any) => [...prevList, {question: question, answer: answer}])
+        localStorage.setItem("list", JSON.stringify(list));
+        setQuestion('');
+        setAnswer('');
     }
+
+    function resetList() {
+        setlist([]);
+        localStorage.removeItem("list");
+    }
+
     function Getanswerbutton(){
         if(question === undefined || question === ''){
             return(<button className='prompt-button' ><img className='arrow' src={arrowIcon} alt='submit answer button'/></button>)
@@ -27,19 +44,21 @@ function Mainpage() {
         )
     }
     return (
-        <body>
+        <div>
         <Header/>
             <div className='answer-area  justify-content-center align-items-center'>
                 <ul style={{listStyle: 'none'}}>
-                    {list.map((item,index)  => (<li key={index}> {item.question}
-                        {item.answer} <hr/> </li> ))}
+                    {list.map((item,index)  => (<li key={index}> <Questionmodal question={item.question}/>
+                        <p>Answer: {item.answer}</p> <hr/> </li> ))}
                     </ul>
                 </div>
                 <div className='d-flex justify-content-center '>
                     <input className='input-bar' placeholder='Write your question here!' value={question} onChange={(e) => setQuestion(e.target.value) }/>
                     <Getanswerbutton/>
                 </div>
-        </body>
+            <button className='reset-button' onClick={resetList}>Reset conversation</button>
+
+        </div>
 
 
 
