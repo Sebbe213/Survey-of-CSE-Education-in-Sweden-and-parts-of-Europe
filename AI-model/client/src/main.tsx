1d4210c43e97fc596e171b573791c555e8044506
Sebbe213 <sebastianraoof@hotmail.se>
2025-05-08 18:44:28 +0200

    Comments

--- AI-model/client/src/main.tsx
+++ AI-model/client/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 //import './index.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Mainpage from './mainpage.tsx'
-
+//main-react-component
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
    <BrowserRouter>
