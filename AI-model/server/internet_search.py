456aca2c4df864ea0dbf57a2d7ff43b7d4c8d7fa
Sebbe213 <sebastianraoof@hotmail.se>
2025-05-13 11:54:51 +0200

    Made some more changes to the duckduckGO code, now it shows not errors.

--- AI-model/server/internet_search.py
+++ AI-model/server/internet_search.py
@@ -1,15 +1,23 @@
+from warnings import catch_warnings
+
 from duckduckgo_search import DDGS
 
 
 def search(query):
     text = ""
     ddgs = DDGS()
-    if(len(query) > 500):
+    try:
+        if(len(query) > 500):
+            query = "Nothing found on the internet!"
+            return query
+        else:
+            results = ddgs.text(query, max_results=2)
+            for data in results:
+                text += " " + str(data)
+            return results
+    except():
         query = "Nothing found on the internet!"
+        print("Error has happened")
         return query
-    else:
-        results = ddgs.text(query, max_results=2)
-        for data in results:
-            text += " " + str(data)
-        return results
+
 
